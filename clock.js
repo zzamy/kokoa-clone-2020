d50ab32ad03b01409658e230d2a7f352891a9c9b
zzamy <hatank1013@gmail.com>
2021-08-15 08:51:16 +0530

    second_remove

--- clock.js
+++ clock.js
@@ -4,8 +4,7 @@ function getClock() {
   const date = new Date();
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
-  const seconds = String(date.getSeconds()).padStart(2, "0");
-  clock.innerText = `${hours}:${minutes}:${seconds}`;
+  clock.innerText = `${hours}:${minutes}`;
 }
 
 getClock();
